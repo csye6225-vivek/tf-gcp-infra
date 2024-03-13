cf31dae00c2c8570fda9a58de6886bf0c23b1371
sai-vivekanand <vangala.sa@northeastern.edu>
2024-03-13 05:18:41 -0400

    private service access

--- main.tf
+++ main.tf
@@ -1,60 +1,62 @@
 provider "google" {
+  #credentials = file(var.credentials_file)
   project = var.project_id
   region  = var.region
 }
 
-resource "google_compute_network" "vpc" {
-  count                           = var.vpc_count
-  name                            = "my-vpc-${count.index}"
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+/*provider "random" {
+  version = "~> 3.0"
+} */
+
+resource "google_compute_network" "vpc_network" {
+  name                            = "vpc-${var.environment}"
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
   routing_mode                    = "REGIONAL"
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  count         = var.vpc_count
-  name          = "webapp-${count.index}"
-  ip_cidr_range = "10.0.${count.index * 2 + 1}.0/24"
+  name          = "webapp-${var.environment}"
+  ip_cidr_range = var.webapp_subnet_cidr
   region        = var.region
-  network       = google_compute_network.vpc[count.index].id
+  network       = google_compute_network.vpc_network.self_link
 }
 
 resource "google_compute_subnetwork" "db" {
-  count         = var.vpc_count
-  name          = "db-${count.index}"
-  ip_cidr_range = "10.0.${count.index * 2 + 2}.0/24"
+  name          = "db-${var.environment}"
+  ip_cidr_range = var.db_subnet_cidr
   region        = var.region
-  network       = google_compute_network.vpc[count.index].id
+  network       = google_compute_network.vpc_network.self_link
 }
 
 resource "google_compute_route" "internet_gateway" {
-  count            = var.vpc_count
-  name             = "internet-gateway-${count.index}"
+  name             = "igw-route-${var.environment}"
   dest_range       = "0.0.0.0/0"
-  network          = google_compute_network.vpc[count.index].id
+  network          = google_compute_network.vpc_network.id
   next_hop_gateway = "default-internet-gateway"
 }
 
 resource "google_compute_firewall" "allow_application_traffic" {
-  count   = var.vpc_count
-  name    = "allow-app-traffic-${count.index}"
-  network = google_compute_network.vpc[count.index].self_link
-  #direction   = "INGRESS"
-  #priority    = 1000
-  target_tags = ["webapp-server"] # Ensure your instance has this tag
+  name    = "allow-app-traffic-${var.environment}"
+  network = google_compute_network.vpc_network.self_link
 
   allow {
     protocol = "tcp"
     ports    = ["8080"] # Replace with your application's port
   }
 
-  source_ranges = ["0.0.0.0/0"] # Allow from any IP
+  source_ranges = ["0.0.0.0/0"]     # Allow from any IP
+  target_tags   = ["webapp-server"] # Ensure your instance has this tag
 }
 
 resource "google_compute_firewall" "deny_ssh" {
-  count     = var.vpc_count
-  name      = "deny-ssh-${count.index}"
-  network   = google_compute_network.vpc[count.index].self_link
+  name      = "deny-ssh-${var.environment}"
+  network   = google_compute_network.vpc_network.self_link
   direction = "INGRESS"
   priority  = 1000
 
@@ -66,32 +68,148 @@ resource "google_compute_firewall" "deny_ssh" {
   source_ranges = ["0.0.0.0/0"] # Deny from any IP
 }
 
-/*data "google_compute_image" "webapp_latest" {
-  project = var.project_id
-  family  = "java-app-fam" # Ensure your Packer images are part of an image family
-  most_recent = true
-} */
+resource "google_project_service" "service_networking" {
+  service            = "servicenetworking.googleapis.com"
+  disable_on_destroy = false
+}
 
-resource "google_compute_instance" "webapp_instance" {
+resource "google_compute_global_address" "private_ip_address" {
+  provider      = google-beta
+  name          = "mysql-private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider                = google-beta
+  network                 = google_compute_network.vpc_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "mysql_instance" {
+  name             = var.instance_name
+  database_version = "MYSQL_5_7" # or the version you want to use
+
+  settings {
+    tier              = var.instance_tier
+    availability_type = "REGIONAL"
+    disk_autoresize   = var.disk_autoresize
+    disk_size         = 100
+    disk_type         = "PD_SSD"
+
+    backup_configuration {
+      binary_log_enabled = true // Enable binary logging for HA
+      enabled            = var.backup_enabled
+    }
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc_network.self_link
+    }
+
+    location_preference {
+      zone = var.zone
+    }
+  }
+
+  deletion_protection = false
+  depends_on          = [google_service_networking_connection.private_vpc_connection]
+}
+
+resource "google_sql_database" "webapp_db" {
+  name     = "webapp"
+  instance = google_sql_database_instance.mysql_instance.name
+}
+
+resource "random_password" "password" {
+  length  = 16
+  special = true
+}
+
+resource "google_sql_user" "webapp_user" {
+  name     = "webapp"
+  instance = google_sql_database_instance.mysql_instance.name
+  password = random_password.password.result
+}
+
+# Assuming you have already defined a CloudSQL instance named `google_sql_database_instance.mysql_instance`
+
+# Create a Google Compute Engine instance with a startup script
+/*resource "google_compute_instance" "webapp_instance" {
   name         = "webapp-instance"
-  machine_type = "n1-standard-1" # Adjust as necessary
-  zone         = "us-east1-b"    # Adjust as necessary
+  machine_type = "n1-standard-1"
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      #image = data.google_compute_image.webapp_latest.self_link
-       image = var.image_name
+      image = var.image_name
     }
   }
 
   network_interface {
-    network    = google_compute_network.vpc[0].self_link
-    subnetwork = google_compute_subnetwork.webapp[0].self_link
-
+    network = google_compute_network.vpc_network.self_link
     access_config {
       // Ephemeral public IP
     }
   }
 
+  metadata_startup_script = file("${path.module}/startup-script.sh")
+} */
+
+# Startup script to configure the web application
+resource "google_compute_instance" "webapp_instance" {
+  name         = "webapp-instance-${var.environment}"
+  machine_type = "n1-standard-1" # Adjust as necessary
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image_name
+    }
+  }
+
+  metadata = {
+    startup-script = <<-EOT
+    #!/bin/bash
+
+    # Exit on any error
+    set -e
+
+    # Wait for the Cloud SQL instance to be created and get the private IP address
+    DB_HOSTNAME="$(gcloud sql instances describe ${google_sql_database_instance.mysql_instance.name} --format='get(ipAddresses[0].ipAddress)')"
+
+    # Wait for the random password to be generated
+    DB_PASSWORD="${random_password.password.result}"
+
+    # Create the .env file with the necessary environment variables
+    cat > /opt/.env <<EOF2
+    DB_HOSTNAME=$DB_HOSTNAME
+    DB_USERNAME=webapp
+    DB_PASSWORD=$DB_PASSWORD
+    EOF2
+
+    # Signal that the startup script has finished
+    touch /var/run/startup-script-completed
+
+    # The rest of your startup script...
+    EOT
+  }
+
+
+  network_interface {
+    network    = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.webapp.self_link
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  service_account {
+    scopes = ["cloud-platform"]
+  }
   tags = ["webapp-server"] # Matches the firewall rule target
+  #metadata_startup_script = file("${path.module}/startup-script.sh")
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,63 @@
+/*variable "credentials_file" {
+  description = "Path to the Google Cloud credentials file"
+}*/
+
 variable "project_id" {
-  description = "The project ID"
-  type        = string
+  description = "The project ID to host resources in"
 }
 
 variable "region" {
-  description = "The region"
-  type        = string
+  description = "The region where resources will be created"
 }
 
-variable "vpc_count" {
-  description = "Number of VPCs to create"
-  type        = number
-  default     = 1
+variable "environment" {
+  description = "A unique name for the environment"
+}
+
+variable "webapp_subnet_cidr" {
+  description = "The CIDR block for the webapp subnet"
+}
+
+variable "db_subnet_cidr" {
+  description = "The CIDR block for the db subnet"
 }
 
 variable "image_name" {
   description = "Name of the custom image"
-  type        = string
 }
+
+variable "zone" {
+  description = "Name of the zone"
+}
+
+variable "instance_name" {
+  description = "The name of the CloudSQL instance"
+  type        = string
+  default     = "my-mysql-instance"
+}
+
+variable "instance_tier" {
+  description = "The machine type for the CloudSQL instance"
+  type        = string
+  default     = "db-n1-standard-1"
+}
+
+variable "disk_autoresize" {
+  description = "Configuration to auto-resize the disk"
+  type        = bool
+  default     = true
+}
+
+variable "backup_enabled" {
+  description = "Whether backups are enabled for the CloudSQL instance"
+  type        = bool
+  default     = true
+}
+
+/*variable "private_network" {
+  description = "The self link of the VPC for the CloudSQL instance"
+  type        = string
+  // This should be the actual self_link of your custom VPC
+  default     = "projects/my-project/global/networks/vpc-dev"
+}*/
 
