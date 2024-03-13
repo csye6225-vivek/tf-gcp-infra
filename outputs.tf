ac1ac2ced304715eb201bf7e23b18bf3b74737b7
sai-vivekanand <141432630+sai-vivekanand@users.noreply.github.com>
2024-03-13 05:21:47 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,1 @@
-output "vpc_id" {
-  value = [for vpc in google_compute_network.vpc : vpc.id]
-}
 
-output "webapp_subnet_id" {
-  value = [for subnet in google_compute_subnetwork.webapp : subnet.id]
-}
-
-output "db_subnet_id" {
-  value = [for subnet in google_compute_subnetwork.db : subnet.id]
-}
